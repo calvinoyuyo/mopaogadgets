406b5a892db2147ee7741e7f2cb78954ac445abe
oyuyes <oyuyocalvin@gmail.com>
2025-07-11 14:51:08 +0300

    fix html

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,14 +26,16 @@ export default async function RootLayout({
 
   const session = await getServerSession();
   return (
-    <body className={`${roboto.className} font-roboto bg-mopao-bg`}>
-      <SessionProvider session={session}>
-        <Header />
-        <Providers>
-          {children}
-        </Providers>
-        <Footer />
-      </SessionProvider>
-    </body>
+    <html lang="en">
+      <body className={`${roboto.className} font-roboto bg-mopao-bg`}>
+        <SessionProvider session={session}>
+          <Header />
+          <Providers>
+            {children}
+          </Providers>
+          <Footer />
+        </SessionProvider>
+      </body>
+    </html>
   );
 }
